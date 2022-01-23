e7792ceb85f0d69f4885be43fc4157ad59f11edd
Ahmed Helow <ahmedhelow@live.com>
2022-01-22 22:25:38 -0800

    Completed add function. "Not tested yet"

--- src/main/java/datastructures/worklists/MinFourHeapComparable.java
+++ src/main/java/datastructures/worklists/MinFourHeapComparable.java
@@ -3,6 +3,10 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.PriorityWorkList;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.lang.Math;
+
 /**
  * See cse332/interfaces/worklists/PriorityWorkList.java
  * for method specifications.
@@ -10,24 +14,51 @@ import cse332.interfaces.worklists.PriorityWorkList;
 public class MinFourHeapComparable<E extends Comparable<E>> extends PriorityWorkList<E> {
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
+    private int size;
+    private int capacity;
 
     public MinFourHeapComparable() {
-        throw new NotYetImplementedException();
+        this.size=0;
+        this.capacity=10;
+        this.data = (E[]) new Object[capacity];
+
+
     }
 
     @Override
     public boolean hasWork() {
-        throw new NotYetImplementedException();
+
+        if(size>=0){
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        if (size==capacity){
+            this.data= Arrays.copyOf(data, capacity*2);
+            this.capacity*=2;
+        }
+         this.data[size]=work;
+        this.size++;
+        int index = size-1;
+        while (Math.ceil((index-4.0)/4.0) >=0 && data[(int) Math.ceil((index-4.0)/4.0)].compareTo(data[(int) index]) >0){
+            E temp = data[index];
+            this.data[index]= this.data[(int) Math.ceil((index-4.0)/4.0)];
+            this.data[(int) Math.ceil((index-4.0)/4.0)] = temp;
+            index =(int) Math.ceil((index-4.0)/4.0);
+        }
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if (!hasWork()) throw new NoSuchElementException();
+        E item= this.data[0];
+        this.data[0]=data[size-1];
+        this.data[size-1]=null;
+        this.size--;
+        return item;
     }
 
     @Override
