4f793373b785c9d14801f3e83a68e09d156b3915
Ahmed Helow <ahmedhelow@live.com>
2022-01-24 20:06:44 -0800

    Completed MoveToFrontList functions. Passed all tests.

--- src/main/java/datastructures/dictionaries/MoveToFrontList.java
+++ src/main/java/datastructures/dictionaries/MoveToFrontList.java
@@ -5,6 +5,7 @@ import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.misc.DeletelessDictionary;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * 1. The list is typically not sorted.
@@ -20,46 +21,44 @@ import java.util.Iterator;
  * dictionary/list's iterator.
  */
 public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
-    private int size;
-    private ListNode head, tail;
+    //    private int size;
+    private ListNode head;
 
-    public MoveToFrontList(){
-        this.size=0;
-        this.head= new ListNode<>(null,null);
-        this.tail=new ListNode(null,null);
+    public MoveToFrontList() {
+        this.size = 0;
+        this.head = new ListNode<>(null, null);
+//        this.tail=new ListNode(null,null);
     }
 
-    public static class ListNode<K,V> {
+    public static class ListNode<K, V> {
         public K key;
         public V value;
-        public ListNode<K,V> next;
-        ListNode(K key, V value){
-            this.key=key;
-            this.value=value;
-            this.next=null;
+        public ListNode<K, V> next;
+
+        ListNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
         }
     }
 
     @Override
     public V insert(K key, V value) {
-        if (key==null || value==null) throw new IllegalArgumentException();
-        ListNode<K,V> temp = new ListNode<>(key,value);
-        if (size==0) {
-            head = tail = temp;
+        if (key == null || value == null) throw new IllegalArgumentException();
+        ListNode<K, V> temp = new ListNode<>(key, value);
+        if (size == 0) {
+            head = temp;
             this.size++;
             return value;
-        }
-        else {
-            V findValue= this.find(key);
-            if (findValue ==null){
-                temp.next=this.head;
-                this.head=temp;
+        } else {
+            V findValue = this.find(key);
+            if (findValue == null) {
+                temp.next = this.head;
+                this.head = temp;
                 this.size++;
                 return null;
-            }
-            else {
-                this.head.value=value;
-                this.size++;
+            } else {
+                this.head.value = value;
                 return findValue;
             }
         }
@@ -68,13 +67,57 @@ public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
     @Override
     public V find(K key) {
 
-        if (key==null) throw new IllegalArgumentException();
+        if (key == null) throw new IllegalArgumentException();
+
+        if (this.size == 0) return null;
+//        if (this.size == 1 && head.key.equals(key)) return (V) head.value;
+
+        ListNode preNode = null;
+        ListNode currentNode = this.head;
+
+        while (currentNode != null) {
+
+            if (currentNode.key.equals(key)) {
+
+                if(preNode != null) {
+                    preNode.next = currentNode.next;
+                    currentNode.next = head;
+                    head = currentNode;
+                }
+
+                return (V) head.value;
+            }
+            preNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        return null;
 
     }
 
     @Override
     public Iterator<Item<K, V>> iterator() {
 
-        throw new NotYetImplementedException();
+        return new Iterator<Item<K, V>>() {
+            ListNode currentNode = head;
+            @Override
+            public boolean hasNext() {
+                if(currentNode == null)
+                    return false;
+                else if(currentNode != null)
+                    return true;
+                else
+                    return false;
+            }
+
+            @Override
+            public Item<K, V> next() {
+                if(this.hasNext()){
+                    Item item = new Item<K, V>((K)currentNode.key, (V)currentNode.value);
+                    currentNode = currentNode.next;
+                    return item;
+                }else
+                    return null;
+            }
+        };
     }
 }
