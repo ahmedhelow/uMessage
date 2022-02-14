f9f80ccf6ce57264bfaae5f8d61232c692e877ad
Yusuf <yhanafy18@gmail.com>
2022-02-13 20:43:49 -0800

    hash trie map, still need to fix 2/12 tests

--- src/main/java/datastructures/dictionaries/AVLTree.java
+++ src/main/java/datastructures/dictionaries/AVLTree.java
@@ -2,6 +2,7 @@ package datastructures.dictionaries;
 
 import cse332.datastructures.trees.BinarySearchTree;
 
+
 /**
  * AVLTree must be a subclass of BinarySearchTree<E> and must use
  * inheritance and calls to superclass methods to avoid unnecessary
@@ -27,5 +28,99 @@ import cse332.datastructures.trees.BinarySearchTree;
  */
 
 public class AVLTree<K extends Comparable<? super K>, V> extends BinarySearchTree<K, V> {
-    // TODO: Implement me!
+    public AVLTree() {
+        super();
+    }
+    public class AVLNode extends BSTNode {
+        int height;
+        public AVLNode(K key, V value) {
+            super(key, value);
+            this.height = 0;
+        }
+    }
+    @Override
+    public V insert(K key, V value) {
+        if (key == null || value == null) {throw new IllegalArgumentException();}
+        V oldValue = find(key);
+        this.root=insert((AVLNode)this.root, key, value);
+        return oldValue;
+    }
+    public AVLNode insert(AVLNode node, K key, V value) {
+        if (node == null) {
+            this.size++;
+            return new AVLNode(key, value);
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.children[0] = insert((AVLNode) node.children[0], key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.children[1] = insert((AVLNode) node.children[1], key, value);
+        } else {
+            node.value = value;
+            return node;
+        }
+
+        node.height = Math.max(height((AVLNode) node.children[0]), height((AVLNode) node.children[1])) + 1;;
+        int i = checkBalance(node);
+
+        if (i > 1 && key.compareTo(node.children[0].key) < 0)
+            return rightRotate(node);
+
+        if (i < -1 && key.compareTo((node.children[1]).key) > 0)
+            return leftRotate(node);
+
+        if (i > 1 && key.compareTo((node.children[0]).key) > 0) {
+            node.children[0] = leftRotate((AVLNode) node.children[0]);
+            return rightRotate(node);
+        }
+
+        if (i < -1 && key.compareTo((node.children[1]).key) < 0) {
+            node.children[1] = rightRotate((AVLNode) node.children[1]);
+            return leftRotate(node);
+        }
+
+        return node;
+
+    }
+    public int height(AVLNode node) {
+        if (node == null) {
+            return -1;
+        } else {
+            return node.height;
+        }
+    }
+
+    public int checkBalance(AVLNode node) {
+        if (node == null) {
+            return -1;
+        } else {
+            return height((AVLNode) node.children[0]) - height((AVLNode) node.children[1]);
+        }
+
+    }
+
+    public AVLNode rightRotate(AVLNode node){
+        AVLNode left = (AVLNode) node.children[0];
+        AVLNode right = (AVLNode) left.children[1];
+
+        left.children[1] = node;
+        node.children[0] = right;
+
+        node.height = Math.max(height((AVLNode) node.children[0]), height((AVLNode) node.children[1])) + 1;
+        left.height = Math.max(height((AVLNode) left.children[0]), height((AVLNode) left.children[1])) + 1;
+
+        return left;
+    }
+
+    public AVLNode leftRotate(AVLNode node){
+        AVLNode left = (AVLNode) node.children[1];
+        AVLNode right = (AVLNode) left.children[0];
+
+        left.children[0] = node;
+        node.children[1] = right;
+
+        node.height = Math.max(height((AVLNode) node.children[0]), height((AVLNode) node.children[1])) + 1;
+        left.height = Math.max(height((AVLNode) left.children[0]), height((AVLNode) left.children[1])) + 1;
+
+        return left;
+    }
 }
